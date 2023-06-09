716998d2e537966917ff8fc5f22ef69861d2aa63
Axel Lopez <l.axel.t@gmail.com>
2023-06-09 10:33:24 -0500

    Corrección header

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -1,8 +1,8 @@
 import "./Header.css"
-function header(){
+function Header(){
     return <header className="header">
         <img src="./img/header.png" alt="header org"></img>
     </header> 
 }
 
-export default header
+export default Header
